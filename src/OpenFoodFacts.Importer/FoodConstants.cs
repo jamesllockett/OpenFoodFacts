827b09d610795dc8991af6e306a293d3b810dac5
James Lockett <James.Lockett@property-tectonics.co.uk>
2022-02-08 09:27:33 +0000

    Defined more constants Added model classes

--- src/OpenFoodFacts.Importer/FoodConstants.cs
+++ src/OpenFoodFacts.Importer/FoodConstants.cs
@@ -26,65 +26,65 @@ namespace OpenFoodFacts.Importer
         public const string HeaderBrandTags = "brands_tags";
         public const string HeaderCategories = "categories";
         public const string HeaderCategoryTags = "categories_tags";
-        /*
-                "categories_en",
-                "origins",
-                "origins_tags",
-                "origins_en",
-                "manufacturing_places",
-                "manufacturing_places_tags",
-                "labels",
-                "labels_tags",
-                "labels_en",
-                "emb_codes",
-                "emb_codes_tags",
-                "first_packaging_code_geo",
-                "cities",
-                "cities_tags",
-                "purchase_places",
-                "stores",
-                "countries",
-                "countries_tags",
-                "countries_en",
-                "ingredients_text",
-                "allergens",
-                "allergens_en",
-                "traces",
-                "traces_tags",
-                "traces_en",
-                "serving_size",
-                "serving_quantity",
-                "no_nutriments",
-                "additives_n",
-                "additives",
-                "additives_tags",
-                "additives_en",
-                "ingredients_from_palm_oil_n",
-                "ingredients_from_palm_oil",
-                "ingredients_from_palm_oil_tags",
-                "ingredients_that_may_be_from_palm_oil_n",
-                "ingredients_that_may_be_from_palm_oil",
-                "ingredients_that_may_be_from_palm_oil_tags",
-                "nutriscore_score",
-                "nutriscore_grade",
-                "nova_group",
-                "pnns_groups_1",
-                "pnns_groups_2",
-                "food_groups",
-                "food_groups_tags",
-                "food_groups_en",
-                "states",
-                "states_tags",
-                "states_en",
-                "brand_owner",
-                "ecoscore_score_fr",
-                "ecoscore_grade_fr",
-                "main_category",
-                "main_category_en",
-                "image_url",
-                "image_small_url",
-                "image_ingredients_url",
-                "image_ingredients_small_url",
+        public const string HeaderCategoriesEn = "categories_en";
+        public const string HeaderOrigins = "origins";
+        public const string HeaderOriginTags = "origins_tags";
+        public const string HeaderOriginsEn = "origins_en";
+        public const string HeaderManufacturingPlaces = "manufacturing_places";
+        public const string HeaderManufacturingPlaceTags = "manufacturing_places_tags";
+        public const string HeaderLabels = "labels";
+        public const string HeaderLabelTags = "labels_tags";
+        public const string HeaderLabelsEnd = "labels_en";
+        public const string HeaderEMBCodes = "emb_codes";
+        public const string HeaderEMBCodeTags = "emb_codes_tags";
+        public const string HeaderFirstPackagingCodeGeo = "first_packaging_code_geo";
+        public const string HeaderCities = "cities";
+        public const string HeaderCityTags = "cities_tags";
+        public const string HeaderPurchasePlaces = "purchase_places";
+        public const string HeaderStores = "stores";
+        public const string HeaderCountries = "countries";
+        public const string HeaderCountryTags = "countries_tags";
+        public const string HeaderCountriesEn = "countries_en";
+        public const string HeaderIngredientsText = "ingredients_text";
+        public const string HeaderAllergens = "allergens";
+        public const string HeaderAllergensEn = "allergens_en";
+        public const string HeaderTraces = "traces";
+        public const string HeaderTraceTags = "traces_tags";
+        public const string HeaderTracesEn = "traces_en";
+        public const string HeaderServingSize = "serving_size";
+        public const string HeaderServingQuantity = "serving_quantity";
+        public const string HeaderNutrientNumber = "no_nutriments";
+        public const string HeaderAdditivesNumber = "additives_n";
+        public const string HeaderAdditives = "additives";
+        public const string HeaderAdditiveTags = "additives_tags";
+        public const string HeaderAdditivesEn = "additives_en";
+        public const string HeaderIngredientsPalmOilNumber = "ingredients_from_palm_oil_n";
+        public const string HeaderIngredientsPalmOil = "ingredients_from_palm_oil";
+        public const string HeaderIngredientsPalmOilTags = "ingredients_from_palm_oil_tags";
+        public const string HeaderPotentialIngredientsPalmOilNumber = "ingredients_that_may_be_from_palm_oil_n";
+        public const string HeaderPotentialIngredientsPalmOil = "ingredients_that_may_be_from_palm_oil";
+        public const string HeaderPotentialIngredientsPalmOilTags = "ingredients_that_may_be_from_palm_oil_tags";
+        public const string HeaderNutriscore = "nutriscore_score";
+        public const string HeaderNutriscoreGrade = "nutriscore_grade";
+        public const string HeaderNovaGroup = "nova_group";
+        public const string HeaderPNNSGroup1 = "pnns_groups_1";
+        public const string HeaderPNNSGroup2 = "pnns_groups_2";
+        public const string HeaderFoodGroups = "food_groups";
+        public const string HeaderFoodGroupTags = "food_groups_tags";
+        public const string HeaderFoodGroupsEn = "food_groups_en";
+        public const string HeaderStates = "states";
+        public const string HeaderStateTags = "states_tags";
+        public const string HeaderStatesEn = "states_en";
+        public const string HeaderBrandOwner = "brand_owner";
+        public const string HeaderEcoScoreFr = "ecoscore_score_fr";
+        public const string HeaderEcoScoreGradeFr = "ecoscore_grade_fr";
+        public const string HeaderMainCategory = "main_category";
+        public const string HeaderMainCategoryEn = "main_category_en";
+        public const string HeaderImageURL = "image_url";
+        public const string HeaderImageURLSmall = "image_small_url";
+        public const string HeaderIngredientsImageURL = "image_ingredients_url";
+        public const string HeaderIngredientsImageURLSmall = "image_ingredients_small_url";
+
                 "image_nutrition_url",
                 "image_nutrition_small_url",
                 "energy-kj_100g",
--- src/OpenFoodFacts.Model/FoodRecord.cs
+++ src/OpenFoodFacts.Model/FoodRecord.cs
@@ -4,12 +4,24 @@ namespace OpenFoodFacts.Model
 {
     public class FoodRecord
     {
+        public FoodRecord()
+        {
+            Nutrition = new Nutrition();
+            Ingredients = new Ingredients();
+        }
+
         public string Code { get; set; }
         public string ProductName { get; set; }
         public string ProductNameAbbreviated { get; set; }
+        public string BrandOwner { get; set; }
+        public string Category { get; set; }
+
         public int CreatedUnix { get; set; }
         public DateTime? Created { get; set; }
         public int LastModifiedUnix { get; set; }
         public DateTime? LastModified { get; set; }
+
+        public Nutrition Nutrition { get; set; }
+        public Ingredients Ingredients { get; set; }
     }
 }
