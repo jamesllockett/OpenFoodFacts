320e2dfc6368b0d1059af0e64d14434ac6bc2980
James Lockett <jllockett@hotmail.com>
2022-02-10 20:50:40 +0000

    Importer UI

---
+++ src/OpenFoodFacts.Importer.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenFoodFacts.Importer.UI.ViewModels
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        public string[] Importers { get; set; }
+        public string[] Targets { get; set; }
+
+        private bool isIdle;
+
+
+        public string SelectedImporter { get; set; }
+        public string SelectedExporter { get; set; }
+
+        public bool IsIdle
+        {
+            get { return isIdle; }
+            set { isIdle = value; NotifyPropertyChanged(nameof(IsIdle)); }
+        }
+
+        private ViewModelBase? importViewModel;
+
+        public ViewModelBase? ImportViewModel
+        {
+            get { return importViewModel; }
+            set { importViewModel = value; NotifyPropertyChanged(nameof(ImportViewModel)); }
+        }
+
+        private ViewModelBase? exportViewModel;
+
+        public ViewModelBase? ExportViewModel
+        {
+            get { return exportViewModel; }
+            set { exportViewModel = value; }
+        }
+
+        public MainViewModel()
+        {
+            Importers = new string[]
+            {
+                "CSV"
+            };
+
+            Targets = new string[]
+            {
+                "SQL Database"
+            };
+
+            IsIdle = true;
+            SelectedExporter = "SQL Database";
+            SelectedImporter = "CSV";
+            ImportViewModel = new FileImportViewModel();
+            ExportViewModel = new ConnectionStringViewModel();
+        }
+    }
+}
